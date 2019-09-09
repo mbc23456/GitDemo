cb9a207ef96a5c86218f10ee7011560d54f8468e
mbc23456 <1659212206@qq.com>
2019-09-09 17:16:55 +0800

    注册登录实现

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import org.jetbrains.annotations.Nullable;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText userName, password;
+    private Button btnLogin;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initView();
+        // initToolbar();
+        initData();
+    }
+
+    private void initData() {
+        String username = readPref();
+        if (!TextUtils.isEmpty(username)) {
+            userName.setText(username);
+        }
+    }
+
+    private String readPref() {
+        SharedPreferences sp = getSharedPreferences("userInfo", MODE_PRIVATE);
+        return sp.getString("username", "");
+    }
+
+    private void initView() {
+        userName = findViewById(R.id.et_user_name);
+        password = findViewById(R.id.et_psw);
+
+        TextView tvRegister = findViewById(R.id.tv_register);
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+//    private void initToolbar() {
+//        Toolbar toolbar=findViewById(R.id.title_toolbar);
+//        toolbar.setTitle("登录");
+//        setSupportActionBar(toolbar);
+//
+//        ActionBar actionBar=getSupportActionBar();
+//        if (actionBar!=null){
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//        }
+//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                LoginActivity.this.finish();
+//            }
+//        });
+//       }
+}
